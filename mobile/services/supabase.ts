a18480556e5464a96954c8df369572e7bd7c591e
bandit.studios <banditstudiosdpd@gmail.com>
2025-06-15 21:19:41 -0400

    Applied models + functions for mobile

---
+++ mobile/services/supabase.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
---
+++ mobile/types/navigation/index.ts
@@ -0,0 +1,25 @@
+import { BlogPost } from "..";
+
+// Navigation types
+export type RootStackParamList = {
+    Auth: undefined;
+    Main: undefined;
+  };
+  
+  export type AuthStackParamList = {
+    Login: undefined;
+    Signup: undefined;
+  };
+  
+  export type MainTabParamList = {
+    Home: undefined;
+    BlogList: undefined;
+    Topics: undefined;
+    Settings: undefined;
+  };
+  
+  export type MainStackParamList = {
+    MainTabs: undefined;
+    BlogDetail: { blogPost: BlogPost };
+    Generate: { topicId?: string };
+  };
