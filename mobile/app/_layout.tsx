4233f4721990da230cd9f155df9c169e874c6112
bandit.studios <banditstudiosdpd@gmail.com>
2025-06-26 21:26:12 -0400

    ALT-6 Refactor mobile project configuration and dependencies
    - Deleted `package.json` and `package-lock.json` to remove outdated configurations and streamline dependency management.
    - Updated `mobile/package.json` to include new dependencies such as `@expo/server` and `@react-native-google-signin/google-signin`, enhancing authentication capabilities.
    - Modified `mobile/app.json` to include `expo-web-browser` for improved web integration.
    - Enhanced `mobile/tsconfig.json` with stricter type checking and added support for React Native types, improving type safety.
    - Refactored `mobile/app/_layout.tsx` to implement a more structured layout with `QueryClientProvider` for state management and improved loading handling.
    - Updated authentication functions in `mobile/utils/auth/function.ts` to streamline Google and Apple sign-in processes, ensuring better error handling and user feedback.
    
    These changes aim to optimize the mobile application structure, improve dependency management, and enhance overall performance and user experience.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,97 @@
-import { Stack } from "expo-router";
+// import { useShowToast } from "@/components/ui/toast/useToast";
+import { AuthProvider, useAuth } from "@/context/auth/AuthContext";
+// import "@/global.css";
+// import { ToastService } from "@/services/ToastService";
+// import { ToastProvider } from "@gluestack-ui/toast";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import "react-native-reanimated";
+// import { GluestackUIProvider } from "../components/ui/gluestack-ui-provider";
+// import LoadingScreen from "./components/LoadingScreen";
+
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 300000, // 5 minutes
+      },
+    },
+  });
+
+
+  //TODO IMPLEMEENT showToast + grab functionality from VENT proj
+  // const showToast = useShowToast();
+  // useEffect(() => {
+  //   // Register the toast callback when component mounts
+  //   ToastService.register(showToast);
+    
+  //   // Clean up when component unmounts
+  //   return () => {
+  //     ToastService.unregister();
+  //   };
+  // }, [showToast]);
+
+
+  const [fontsLoaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; // Keep the splash screen visible while fonts load
+  }
+
+  // Render the AuthProvider, once font is loaded
+  return (
+    <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+// Separate component for content after authentication is initialized
+function AppContent() {
+  const { authState } = useAuth();
+  const colorScheme = useColorScheme();
+
+  if (authState.isLoading) {
+    // TODO implement LoadingScreen
+    // return <LoadingScreen />;
+  }
+
+  return (
+    // <GluestackUIProvider mode={colorScheme === "dark" ? 'light' : 'light'}>
+      <ThemeProvider
+        value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        {/* <ToastProvider> */}
+          <Slot />
+          <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+        {/* </ToastProvider> */}
+      </ThemeProvider>
+    // </GluestackUIProvider>
+  );
 }
