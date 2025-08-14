af2c864929cd55d002a872cc8e5e4ffc95063c5d
bandit.studios <banditstudiosdpd@gmail.com>
2025-08-14 15:51:19 -0400

    Added .get to userId field

--- api/app.py
+++ api/app.py
@@ -58,7 +58,7 @@ def research_topic():
             }), 400
         
         topic = data['topic'].strip().upper()
-        userId = data['userId']
+        userId = data.get['userId']
         save_to_db = data.get('save_to_db', True)
         
         # Validate topic
