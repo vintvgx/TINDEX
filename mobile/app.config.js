9a986df1d1344ec2c5908a0152de8c8ffa565115
bandit.studios <banditstudiosdpd@gmail.com>
2026-03-28 13:39:30 -0400

    TDX-77 Epic 3 (0.3.0)

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "alethia",
     slug: "alethia",
-    version: "0.0.32",
+    version: "0.3.0",
     orientation: "portrait",
     icon: "./assets/logo/tindex_logo.png",
     scheme: "mobile",
