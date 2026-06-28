6ed4d2964beaa6c57f949db576e0aed4743e8840
bandit.studios <banditstudiosdpd@gmail.com>
2026-06-28 11:37:22 -0400

    Update version numbers in app.config.js and package.json to 0.3.14 for consistency across the mobile application.

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Tindex",
     slug: "alethia",
-    version: "0.3.12",
+    version: "0.3.14",
     orientation: "portrait",
     icon: "./assets/logo/tindex_spark.png",
     scheme: "mobile",
