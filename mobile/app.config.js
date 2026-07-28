b5888ead008632e25b52b7d2ee69cca0f08b3a36
bandit.studios <banditstudiosdpd@gmail.com>
2026-07-28 01:13:35 -0400

    chore: update mobile app version to 0.4.16 in configuration files
    - Incremented version number in app.config.js and package.json from 0.4.14 to 0.4.16, ensuring consistency across the mobile application.

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Tindex",
     slug: "alethia",
-    version: "0.4.14",
+    version: "0.4.16",
     orientation: "portrait",
     icon: "./assets/logo/tindex_spark.png",
     scheme: "mobile",
