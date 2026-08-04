23a72e41b785c0037333709b1206c65af3f41c1c
bandit.studios <banditstudiosdpd@gmail.com>
2026-08-03 23:38:52 -0400

    0.4.24

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Tindex",
     slug: "alethia",
-    version: "0.4.22",
+    version: "0.4.24",
     orientation: "portrait",
     icon: "./assets/logo/tindex_spark.png",
     scheme: "mobile",
